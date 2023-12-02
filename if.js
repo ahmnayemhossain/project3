e5e17e3a3a0ff3e2a754259cfb267ba1736178c4
ahmnayemhossain <ahmnayemhossain@gmail.com>
2023-12-02 16:15:55 +0600

    first commit

--- if.js
+++ if.js
@@ -1,1 +1,6 @@
+import { sadik } from "./sadik";
+import { nayem } from "./sadik";
+
 console.log("Hello Bangladesh")
+var p= `I am ${nayem} and my nephew's name is ${sadik}`;
+console.log(p)
---
+++ sadik.js
@@ -0,0 +1,2 @@
+export var sadik= 'Sadik Shahariyar Nakib';
+export var nayem= 'A. H. M Nayem Hossain';
