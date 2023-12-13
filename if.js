f01c287da57a6253bf62957092d1a537f77f4902
ahmnayemhossain <ahmnayemhossain@gmail.com>
2023-12-13 20:04:03 +0600

    ancynchronas

--- if.js
+++ if.js
@@ -1,13 +1,38 @@
-function displayer(some) {
-    document.getElementById("demo").innerHTML = some;
+const paymentSuccess = true;
+const marks = 90;
+
+function enroll(callback){
+    document.getElementById('demo1').innerHTML = 'payment progrssing';
+
+    setTimeout(()=>{
+        if(paymentSuccess){
+            callback();
+        }else{
+            document.getElementById('demo2').innerHTML = 'Payment faild';
+        }
+    },200)  
+} 
+
+function progress(callback){
+    document.getElementById('demo3').innerHTML = 'course on progress';
+    setTimeout(() => {
+        if(marks >= 80){
+            callback()
+        } else{
+            document.getElementById('demo4').innerHTML = 'not eligibel for certificate';
+        }
+    },3000);
 }
 
-function calculator(num1, num2, callback) {
-    let sum = num1 + num2;
-    callback(sum);
+function getCertificate(){
+    document.getElementById('demo4').innerHTML = 'Preparing your certificate!';
+
+    setTimeout(() => {
+        document.getElementById('demo4').innerHTML = 'congrat! you got the certificate'
+    }, 1000);
 }
 
-// Using an anonymous function as a callback
-calculator(5, 5, function(result) {
-    displayer(result);
+enroll(()=>{
+    progress(getCertificate)
 });
+// progress(getCertificate);
